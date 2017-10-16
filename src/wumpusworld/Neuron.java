e63fc4bc7c71dcbeffd5bb82d488f6b7158a746d
Unknown <menu111117@gmail.com>
2017-10-16 11:12:45 +0200

    Added constructors for NN and N

--- src/wumpusworld/Neuron.java
+++ src/wumpusworld/Neuron.java
@@ -5,6 +5,17 @@ import java.util.ArrayList;
 public class Neuron {
 	private ArrayList<Float> w;
 	
+	public Neuron() {
+		w = new ArrayList<Float>();
+	}
+	
+	public Neuron(int childs) {
+		w = new ArrayList<Float>();
+		for(int i = 0; i < childs; i++) {
+			w.add((float)(Math.random() * 2) - 1);
+		}
+	}
+	
 	private float sigmoid(float x) {	
 		return (float) (1/((1 + Math.pow(Math.E, (-1*x)))));
 	}
